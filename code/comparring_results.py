8f53a1cda351108686f430cd0565df600d8bfacb
BeniPlut1 <76884887+BeniPlut1@users.noreply.github.com>
2022-05-03 23:43:38 +0200

    Posodobljen PDF

--- code/comparring_results.py
+++ code/comparring_results.py
@@ -1,11 +1,11 @@
 import nltk
 from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
-from transformers import BertTokenizer, TFBertForQuestionAnswering
+from transformers import BertTokenizer, TFBertForQuestionAnswering, pipeline
 import tensorflow as tf
 
 def izracunajF1(prediction, realAnswer):
-    predictionTokeni=nltk.word_tokenize(prediction) #Pridobi tokene za pravi odgovor in napoved modela
+    predictionTokeni=nltk.word_tokenize(prediction) #Pridobi tokene za napoved in napoved modela
     normaliziraniPredictionTokeni=[word for word in predictionTokeni if word.isalpha()] #te tokene normalizira, kar pomeni, da jim odstrani ločila in stop words za vsako besedo
     realAnswerTokeni=nltk.word_tokenize(realAnswer)
     normaliziraniRealAnswerTokeni=[word for word in realAnswerTokeni if word.isalpha()]
@@ -20,15 +20,12 @@ def izracunajF1(prediction, realAnswer):
     return f1, accuracy#2*(precision*recall)/(precision+recall)                      #izracun F1 score
 
 def pridobiPrediction(question, paragraph):
-    tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
-    bert_model = TFBertForQuestionAnswering.from_pretrained('./code/modelSlo')
-    #bert_model = TFBertForQuestionAnswering.from_pretrained("bert-base-cased")
-    question1, text = "Kakšna energija naredi žarilno žarnico?", "Žarnica z žarilno nitko, žarnica ali žarnica z žarilno nitko je električna svetloba z žično nitko, ki se segreje na visoko temperaturo, tako da skozi njo prehaja električni tok, dokler ne sveti z vidno svetlobo (incandescence). Vroč filament je zaščiten pred oksidacijo s stekleno ali kremenčevo žarnico, ki je napolnjena z inertnim plinom ali evakuirana. V halogenski žarnici se izhlapevanje žarilne nitke prepreči s kemičnim postopkom, ki ponovno nalaga kovinsko paro na žarilno nitko in podaljša njeno življenjsko dobo. Žarnica se napaja z električnim tokom s priključki ali žicami, vgrajenimi v steklo. Večina žarnic se uporablja v vtičnici, ki zagotavlja mehansko podporo in električne povezave."
-    inputs = tokenizer(text, question1, return_tensors="tf")
-    outputs = bert_model(inputs)
-    answer_start_index = int(tf.math.argmax(outputs.start_logits, axis=-1)[0])
-    answer_end_index = int(tf.math.argmax(outputs.end_logits, axis=-1)[0])
-    predict_answer_tokens = inputs.input_ids[0, answer_start_index : answer_end_index + 1]
-    print(bert_model)
-    return tokenizer.decode(predict_answer_tokens)
-print(pridobiPrediction("", ""))
+    qamodel=pipeline('question-answering', model=('./code/model'), tokenizer=("bert-base-multilingual-cased"))
+    result=qamodel(question = question, context = paragraph)
+    return result['answer']
+question="In what country is Normandy located?"
+context="The Normans (Norman: Nourmands; French: Normands; Latin: Normanni) were the people who in the 10th and 11th centuries gave their name to Normandy, a region in France. They were descended from Norse ('Norman' comes from 'Norseman') raiders and pirates from Denmark, Iceland and Norway who, under their leader Rollo, agreed to swear fealty to King Charles III of West Francia. Through generations of assimilation and mixing with the native Frankish and Roman-Gaulish populations, their descendants would gradually merge with the Carolingian-based cultures of West Francia. The distinct cultural and ethnic identity of the Normans emerged initially in the first half of the 10th century, and it continued to evolve over the succeeding centuries."
+groundTruths="France"
+prediction=pridobiPrediction(question, context)
+answer=["three dimensional index"]
+print("Question: "+question+"\n\nContext: "+context+"\n\nGround truths: "+groundTruths+"\n\nprediction: "+prediction)
