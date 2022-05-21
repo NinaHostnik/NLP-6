f868fe9fe4eb168a8accef3b87f6729a28820c41
BeniPlut1 <76884887+BeniPlut1@users.noreply.github.com>
2022-05-22 00:21:33 +0200

    Update comparring results in bert eng

--- code/bert_eng.py
+++ code/bert_eng.py
@@ -2,21 +2,26 @@ from datasets import load_dataset
 import tensorflow as tf
 
 from transformers import AutoTokenizer
-from transformers import DefaultDataCollator
+from transformers import DefaultDataCollator, Adafactor
 from transformers import create_optimizer
-from transformers import BertConfig, TFBertForQuestionAnswering
+from transformers import BertConfig, TFBertForQuestionAnswering, TFAutoModelForQuestionAnswering
+from transformers import get_constant_schedule_with_warmup
+from tensorflow import keras
 
-squad_eng = load_dataset('json', data_files={'train': './data/cleaned_ENG_train.json', 'validation': './data/cleaned_ENG_validation.json'}, field='data')
-
-tokenizer = AutoTokenizer.from_pretrained("bert-base-multilingual-cased")
+squad_eng = load_dataset('json', data_files={'train': './code/data/cleaned_ENG_train.json', 'validation': './code/data/cleaned_ENG_validation.json'}, field='data')
+#squad_eng = load_dataset('squad_v2')
+tokenizer = AutoTokenizer.from_pretrained("roberta-base")
 
 
 def preprocess_function(examples):
     questions = [q.strip() for q in examples["question"]]
+    contexts = [c.strip() for c in examples["context"]]
     inputs = tokenizer(
         questions,
-        examples["context"],
+        #examples["context"],
+        contexts,
         max_length=384,
+        stride=128,
         truncation="only_second",
         return_offsets_mapping=True,
         padding="max_length",
@@ -30,14 +35,17 @@ def preprocess_function(examples):
 
     for i, offset in enumerate(offset_mapping):
         answer = answers[i]
+        #print(answer['text'][0])
         ctx = context[i]
-        #start_char = len(ctx) + 1
-        #end_char = len(ctx) + 1
-        #if len(answer) > 0:
-        start_char = ctx.find(answer[0]['text']) if ctx.find(answer[0]['text']) > -1 else len(ctx) + 1
-        end_char = start_char + len(answer[0]['text']) if ctx.find(answer[0]['text']) > -1 else len(ctx) + 1
+        start_char = len(ctx) + 1
+        end_char = len(ctx) + 1     #comment these 2 cause no answerable za proper squad v2
+        #print(answer["text"])
+        if len(answer[0]["text"]) > 0:
+            start_char = ctx.find(answer[0]['text']) if ctx.find(answer[0]['text']) > -1 else len(ctx) + 1         #ns dataset ma mjckn drgac za klic
+            end_char = start_char + len(answer[0]['text']) if ctx.find(answer[0]['text']) > -1 else len(ctx) + 1
+            #start_char = ctx.find(answer['text'][0]) if ctx.find(answer['text'][0]) > -1 else len(ctx) + 1
+            #end_char = start_char + len(answer['text'][0]) if ctx.find(answer['text'][0]) > -1 else len(ctx) + 1
         sequence_ids = inputs.sequence_ids(i)
-
         # Find the start and end of the context
         idx = 0
         while sequence_ids[idx] != 1:
@@ -71,43 +79,52 @@ def preprocess_function(examples):
 tokenized_squad = squad_eng.map(preprocess_function, batched=True, remove_columns=squad_eng["train"].column_names)
 data_collator = DefaultDataCollator(return_tensors="tf")
 
+batch_size = 8
 # define training hyperparameters for training set
 tf_train_set = tokenized_squad["train"].to_tf_dataset(
     columns=["attention_mask", "input_ids", "start_positions", "end_positions"],
     dummy_labels=True,
     shuffle=True,
-    batch_size=8,
+    batch_size=batch_size,
     collate_fn=data_collator,
+    drop_remainder=True,
 )
-
+#tf_train_set = tokenized_squad["train"].with_format("numpy")[:]
 # define training hyperparameters for validation set
 tf_validation_set = tokenized_squad["validation"].to_tf_dataset(
     columns=["attention_mask", "input_ids", "start_positions", "end_positions"],
     dummy_labels=True,
-    shuffle=False,
-    batch_size=8,
+    shuffle=True,
+    batch_size=batch_size,
     collate_fn=data_collator,
+    drop_remainder=True,
+)
+#tf_validation_set =tokenized_squad["validation"].with_format("numpy")[:]
+
+train_data_size=len(tokenized_squad["train"])
+num_epochs = 1
+num_warmup_steps=int(num_epochs * train_data_size * 0.1 / batch_size)
+total_train_steps = (train_data_size // batch_size) * num_epochs
+steps_per_epoch = int(train_data_size / batch_size)
+num_train_steps = steps_per_epoch * num_epochs
+optimizer, schedule = create_optimizer(
+    init_lr=5e-5,
+    num_warmup_steps=0,#num_warmup_steps,
+    num_train_steps=num_train_steps,
+    #min_lr_ratio=0.99,
 )
 
-batch_size = 8
-num_epochs = 1
-total_train_steps = (len(tokenized_squad["train"]) // batch_size) * num_epochs
-optimizer, schedule = create_optimizer(
-    init_lr=2e-5,
-    num_warmup_steps=0,
-    num_train_steps=total_train_steps,
-)
 
 # get pretrained model
-config = BertConfig.from_pretrained("bert-base-multilingual-cased")
-model_eng = TFBertForQuestionAnswering.from_config(config)
+config = BertConfig.from_pretrained("roberta-base")
+#model_eng = TFBertForQuestionAnswering.from_config(config)
+model_eng = TFAutoModelForQuestionAnswering.from_config(config)
 
 model_eng.compile(optimizer=optimizer)
-
 # fine-tune model
-model_eng.fit(x=tf_train_set, validation_data=tf_validation_set, epochs=1)
+model_eng.fit(x=tf_train_set, validation_data=tf_validation_set, epochs=num_epochs, batch_size=batch_size)
 
 # save model
-model_eng.save_pretrained('./code/model/')
+model_eng.save_pretrained('./code/modelShuffleOn/')
 
 
--- code/comparring_results.py
+++ code/comparring_results.py
@@ -1,33 +1,44 @@
 import nltk
-from sklearn.metrics import f1_score
-from sklearn.metrics import accuracy_score
-from transformers import BertTokenizer, TFBertForQuestionAnswering, pipeline
+from transformers import pipeline
 import tensorflow as tf
 import json
+import editdistance
 
 def izracunajF1(prediction, realAnswer):
-    predictionTokeni=nltk.word_tokenize(prediction) #Pridobi tokene za napoved in napoved modela
+    predictionTokeni=nltk.word_tokenize(prediction[0]) #Pridobi tokene za napoved in napoved modela
     normaliziraniPredictionTokeni=[word for word in predictionTokeni if word.isalpha()] #te tokene normalizira, kar pomeni, da jim odstrani ločila in stop words za vsako besedo
-    realAnswerTokeni=nltk.word_tokenize(realAnswer)
+    realAnswerTokeni=nltk.word_tokenize(realAnswer[0])
     normaliziraniRealAnswerTokeni=[word for word in realAnswerTokeni if word.isalpha()]
+    #print(predictionTokeni)
+    #print(realAnswerTokeni)
     truePositive=0
+    falsePositive=0
+    falseNegative=0
     for token in predictionTokeni:
-        if token in realAnswerTokeni:
+        if str(token) in realAnswerTokeni:
             truePositive=truePositive+1
-    precision = truePositive / len(normaliziraniPredictionTokeni)  #izracun precision (pravi najdeni/vsi najdeni)
-    recall = truePositive / len(normaliziraniRealAnswerTokeni)     #izracun recall (pravi najdeni/vsi pravilni odgovori)
-    f1=0
-    #f1=f1_score(normaliziraniPredictionTokeni, normaliziraniRealAnswerTokeni, average='micro')
-    #accuracy=accuracy_score(normaliziraniPredictionTokeni, normaliziraniRealAnswerTokeni)
-    return 2*(precision*recall)/(precision+recall) #f1, accuracy#2*(precision*recall)/(precision+recall)                      #izracun F1 score
+    if truePositive==0:
+        return 0
+    for token in predictionTokeni:
+        if str(token) not in realAnswerTokeni:
+            falsePositive+=1
+    for token in realAnswerTokeni:
+        if str(token) not in predictionTokeni:
+            falseNegative+=1   
+    precision=truePositive/(truePositive+falsePositive)             #izracun precision (pravi najdeni/(pravi najdeni+narobni najdeni))
+    recall=truePositive/(truePositive+falseNegative)                #izracun recall (pravi najdeni/(pravi najdeni+pravi nenajdeni))
+    return 2*(precision*recall)/(precision+recall)                      #izracun F1 score
 
 def pridobiPrediction(question, paragraph, qamodel):
     result=qamodel(question = question, context = paragraph)
     return result['answer']
-#question="In what country is Normandy located?"
-#context="The Normans (Norman: Nourmands; French: Normands; Latin: Normanni) were the people who in the 10th and 11th centuries gave their name to Normandy, a region in France. They were descended from Norse ('Norman' comes from 'Norseman') raiders and pirates from Denmark, Iceland and Norway who, under their leader Rollo, agreed to swear fealty to King Charles III of West Francia. Through generations of assimilation and mixing with the native Frankish and Roman-Gaulish populations, their descendants would gradually merge with the Carolingian-based cultures of West Francia. The distinct cultural and ethnic identity of the Normans emerged initially in the first half of the 10th century, and it continued to evolve over the succeeding centuries."
-#groundTruths="France"
-#prediction=pridobiPrediction(question, context)
+question="In what country is Normandy located?"
+context="The Normans (Norman: Nourmands; French: Normands; Latin: Normanni) were the people who in the 10th and 11th centuries gave their name to Normandy, a region in France. They were descended from Norse ('Norman' comes from 'Norseman') raiders and pirates from Denmark, Iceland and Norway who, under their leader Rollo, agreed to swear fealty to King Charles III of West Francia. Through generations of assimilation and mixing with the native Frankish and Roman-Gaulish populations, their descendants would gradually merge with the Carolingian-based cultures of West Francia. The distinct cultural and ethnic identity of the Normans emerged initially in the first half of the 10th century, and it continued to evolve over the succeeding centuries."
+groundTruths=["about 10,000 years ago"]
+prediction=[]
+prediction.append("10,000")#pridobiPrediction(question, context))
+f1=izracunajF1(prediction, groundTruths)
+#print(f1)
 #answer=["three dimensional index"]
 #print("Question: "+question+"\n\nContext: "+context+"\n\nGround truths: "+groundTruths+"\n\nprediction: "+prediction)
 #import podatkov
@@ -37,27 +48,44 @@ testImport.close()
 testData = json.loads(data_string)
 stPravilnih=0
 stNeprevilnih=0
-qamodel=pipeline('question-answering', model=('./code/model'), tokenizer=("bert-base-multilingual-cased"))
-qamodel2=pipeline('question-answering', model="keras-io/transformers-qa")
+stPredolgih=0
+qamodel=pipeline('question-answering', model="./code/roberta Model 3 epoch lr 5e-5 base preprocess", tokenizer=("roberta-base"))
+#qamodel2=pipeline('question-answering', model="keras-io/transformers-qa")
 i=0
 f1Vsi=[]
 accuracyVsi=[]
+editDistances=[]
 for data in testData['data']:
-    prediction=pridobiPrediction(data['question'], data['context'], qamodel2)
+    prediction=pridobiPrediction(data['question'], data['context'], qamodel)
     realAnswer=data['answers']
-    #f1=izracunajF1(prediction, realAnswer[0]['text'])
+    print("Prediction:")
+    print(prediction)
+    print(len(prediction))
+    print("Ground Truth")
+    print(realAnswer[0]['text'])
+    print(len(realAnswer[0]['text']))
+    f1=izracunajF1([prediction], [realAnswer[0]['text']])
+    editDistance=editdistance.eval(realAnswer[0]['text'], prediction)
+    editDistances.append(editDistance)
+    print("F1 score")
+    print(f1)
+    print("edit distance")
+    print(editDistance)
+    #print(f1)
+    if str(realAnswer) in str(prediction):
+        stPredolgih=stPredolgih+1
     if str(prediction) in str(realAnswer):
         stPravilnih=stPravilnih+1
-        print(prediction)
-        print(realAnswer[0]['text'])
     else:
         stNeprevilnih=stNeprevilnih+1
-    #f1Vsi.append(f1)
-    #accuracyVsi.append(accuracy)
+    f1Vsi.append(f1)
     #print(i)
     i=i+1
     if i==100:
         break
-print(str(stPravilnih)+" "+str(stNeprevilnih))
-print(f1Vsi)
-print(accuracyVsi)
+print(str(stPravilnih)+" "+str(stNeprevilnih)+" "+str(stPredolgih))
+print(sum(f1Vsi)/len(f1Vsi))
+print(sum(editDistances)/len(editDistances))
+print(editDistances)
+
+#print(accuracyVsi)
